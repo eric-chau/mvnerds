154d6c0463b983e30d96ef7d322b06fa8772e01f
Eric Chau <mvnerds.ro0ny@gmail.com>
2012-09-06 16:54:47 +0200

    Project is now initiate; Propel, Twitter Bootstrap and HTML5BoilerPlate are now ready to use

---
+++ src/MVNerds/AdminBundle/MVNerdsAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVNerds\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MVNerdsAdminBundle extends Bundle
+{
+}
---
+++ src/MVNerds/CoreBundle/MVNerdsCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MVNerds\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MVNerdsCoreBundle extends Bundle
+{
+}
