f56a8c31127eb676920fe6bf29d086723acdab6d
Chau Eric <mvnerds.ro0ny@gmail.com>
2012-09-10 18:34:23 +0200

    Ajout du schema.xml dans le CoreBundle; Generation des classes PHP avec Propel dans le dossier Model du CoreBundle; mise a jour de gitignore

---
+++ src/MVNerds/CoreBundle/Model/GameFeed.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MVNerds\CoreBundle\Model;
+
+use MVNerds\CoreBundle\Model\om\BaseGameFeed;
+
+
+/**
+ * Skeleton subclass for representing a row from the 'game_feed' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.MVNerds.CoreBundle.Model
+ */
+class GameFeed extends BaseGameFeed {
+
+} // GameFeed
---
+++ src/MVNerds/CoreBundle/Model/TournamentFormatQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MVNerds\CoreBundle\Model;
+
+use MVNerds\CoreBundle\Model\om\BaseTournamentFormatQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'tournament_format' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.MVNerds.CoreBundle.Model
+ */
+class TournamentFormatQuery extends BaseTournamentFormatQuery {
+
+} // TournamentFormatQuery
