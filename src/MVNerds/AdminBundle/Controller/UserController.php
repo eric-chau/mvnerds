1044245e803def4b4e76fb54cf0e9a74f82b11c0
Chau Eric <mvnerds.ro0ny@gmail.com>
2012-09-13 00:02:21 +0200

    Issue #2 - WIP; Suppression d'utilisateur [OK]; TODO: page d'édition d'un utilisateur

--- src/MVNerds/AdminBundle/Controller/UserController.php
+++ src/MVNerds/AdminBundle/Controller/UserController.php
@@ -4,9 +4,12 @@ namespace MVNerds\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 
 use MVNerds\CoreBundle\Form\Type\UserType;
 use MVNerds\CoreBundle\Model\UserQuery;
+use MVNerds\CoreBundle\Model\UserPeer;
 
 /**
  * @Route("/utilisateurs")
@@ -56,5 +59,28 @@ class UserController extends Controller
         return $this->render('MVNerdsAdminBundle:User:add_user_form.html.twig', array(
             'form' => $form->createView()
         ));
+    }
+
+    /**
+     * Supprimer l'utilisateur $id de la base de données;
+     * TODO: Retrouver l'utilisateur selon son pseudo et non son id
+     *
+     * @Route("/{id}/supprimer", name="admin_users_delete")
+     */
+    public function deleteUserAction($id)
+    {
+        $user = UserQuery::create()
+            ->add(UserPeer::ID, $id)
+        ->findOne();
+
+        if (null === $user)
+        {
+            throw new InvalidArgumentException('User with id:'.$id.' does not exist!');
+        }
+
+        // Finally
+        $user->delete();
+
+        return new Response(json_encode(true));
     }
 }
--- web/js/admin/users.js
+++ web/js/admin/users.js
@@ -1,8 +1,34 @@
 jQuery(function($) {
+	$('.tooltip-anchor').tooltip();
+
 	$(document).ready(function() {
 		$('a.btn-submit-form').click(function(event) {
 			event.preventDefault();
 			$('input[type="submit"].submit-form').trigger('click');
 		});
+
+		$('a.btn-confirm-delete').click(function(event) {
+			event.preventDefault();
+			var $loader = $(this).parent().find('img'), $this = $(this);
+			$loader.removeClass('hide');
+			$.ajax({
+				url: $(this).attr('href'),
+				type: 'GET',
+				datetype: 'json',
+				success: function(response) {
+					if (response) {
+						// On remonte à la modal pour la cachée
+						$this.parent().parent().modal('hide');
+						// Objet jQuery portant sur la ligne qui concerne l'utilisateur
+						var $row = $this.parent().parent().parent();
+						$loader.addClass('hide');
+						$row.slideUp('slow');
+						$row.remove();
+					}
+				}
+			});
+		})
 	});
+
+
 });
