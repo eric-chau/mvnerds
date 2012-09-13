0dc296079efc46cf332cee46d35b435d6b723faf
Haniki <Haniki@Haniki-PC>
2012-09-13 17:57:09 +0200

    Ajout d'un formulaire pour le champion grabber modification du schema.xml pour intégrer le slug sur le nom des champions ajout des images des champions et affichage dans l'administration

---
+++ src/MVNerds/DataGrabberBundle/Form/Type/ChampionGrabberType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MVNerds\DataGrabberBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use \Symfony\Component\Form\FormBuilderInterface;
+
+class ChampionGrabberType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options) 
+	{
+		$builder->add('start_index', 'integer', array(
+			'label'		=> 'Index de départ (Commence à partir de 0)',
+			'required'	=> true,
+			'data'		=> 0
+		));
+		
+		$builder->add('nb_champions', 'integer', array(
+			'label'		=> 'Nombre de champions à récupérer (0 pour récupérer tout ce qui suit l\'index de départ)',
+			'required'	=> true,
+			'data'		=> 0
+		));
+	}
+	
+	public function getName()
+	{
+		return 'champion_grabber';
+	}
+}
