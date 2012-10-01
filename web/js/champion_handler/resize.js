8fb9005fac2f5dd0102085d1e2101d2895f6baad
Haniki <Haniki@Haniki-PC>
2012-10-01 17:26:01 +0200

    Amélioration animations isotope

--- web/js/champion_handler/resize.js
+++ web/js/champion_handler/resize.js
@@ -1,22 +1,11 @@
 /**
  * Permet de gérer l'affichage du détail des champions lors du clic sur leurs miniatures
  */
+
 jQuery(function($) {
 		
-	var options = {
-		itemSelector: '.champion',
-		animationEngine: 'jquery',
-		masonry: {
-			columnWidth: 124
-		},
-		animationOptions: {
-			duration: 400,
-			height: 'hide',
-			width: 'hide',
-			queue: false,
-			opacity: 1
-		}
-	};
+	var options = getIsotopeOptions();
+	
 	$isotope = $('#isotope-list');
 	
 	$isotope.imagesLoaded( function(){
@@ -43,15 +32,6 @@ jQuery(function($) {
 	});
 	
 	function maximizeChampion($champ, $isotope){
-//		$champ.toggleClass('champion-maxi');
-//		$isotope.isotope( 'reLayout', function(){
-//			setTimeout(function(){
-//				scrollToChampion($('#'+$champ.attr('id')))
-//				},
-//				100
-//			);
-//		});
-
 		$champ.find('div.portrait').fadeOut(250);
 		$champ.addClass('champion-maxi');
 		setTimeout(function() 
@@ -94,35 +74,5 @@ jQuery(function($) {
 		var position = $champ.position().top + $('#champion-comparison-center').position().top - 20;
 		console.log($champ.position());
 		$('body,html').animate({scrollTop:position},500);
-	}
-	
-	/*********** FILTER *************/	
-	$.ajax({
-		type: 'GET',
-		url:  Routing.generate('champion_handler_front_get_champions_name'),
-		dataType: 'html'
-	}).done(function(data){
-		$('#filter-value').attr('data-source', data);
-	});
-
-	$('#filter-value').keyup(function(){
-		filter();
-	});
-	$('#filter-value').change(function(){
-		filter();
-	});
-	
-	function filter(){
-		var filterValue = $('#filter-value').val();
-		if(filterValue != '')
-		{
-			options['filter']="[data-name*='" + filterValue.toLowerCase()+"']";
-			$isotope.isotope(options);
-		}
-		else
-		{
-			options['filter']="*";
-			$isotope.isotope(options);
-		}
 	}
 });
