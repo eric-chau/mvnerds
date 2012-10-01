b751f937215e30440cb89e0a147d6306ae7e9e50
Haniki <Haniki@Haniki-PC>
2012-10-01 18:37:47 +0200

    Nettoyage du JS factorisation du code

--- web/js/champion_handler/filter.js
+++ web/js/champion_handler/filter.js
@@ -1,33 +1,41 @@
-$(function(){
-	
-	var options = getIsotopeOptions();
-	
+var options, $isotope = $('#isotope-list'), $filterValue = $('#filter-value');
+
+function initTypeahead($isotope){
+	options =  getIsotopeOptions();
+	setTypeaheadChampionsName();
+	$filterValue.off('keyup change');
+	$filterValue.keyup(function(){
+		filter($isotope);
+	});
+	$filterValue.change(function(){
+		filter($isotope);
+	});
+}
+
+function setTypeaheadChampionsName(){
 	$.ajax({
 		type: 'GET',
 		url:  Routing.generate('champion_handler_front_get_champions_name'),
 		dataType: 'html'
 	}).done(function(data){
-		$('#filter-value').attr('data-source', data);
+		$filterValue.attr('data-source', data);
 	});
+}
 
-	$('#filter-value').keyup(function(){
-		filter();
-	});
-	$('#filter-value').change(function(){
-		filter();
-	});
-	
-	function filter(){
-		var filterValue = $('#filter-value').val();
-		if(filterValue != '')
-		{
-			options['filter']="[data-name*='" + filterValue.toLowerCase()+"']";
-			$isotope.isotope(options);
-		}
-		else
-		{
-			options['filter']="*";
-			$isotope.isotope(options);
-		}
+function filter($isotope){
+	var filterValue = $filterValue.val();
+	if(filterValue != '')
+	{
+		options['filter']="[data-name*='" + filterValue.toLowerCase()+"']";
+		$isotope.isotope(options);
 	}
+	else
+	{
+		options['filter']="*";
+		$isotope.isotope(options);
+	}
+}
+
+$(function(){	
+	initTypeahead($isotope);
 });
