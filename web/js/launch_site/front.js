1c1d9ac4fed19b40021a0b3aacd08e1ab8e4fb66
Chau Eric <mvnerds.ro0ny@gmail.com>
2012-10-09 00:34:06 +0200

    - Création d'un fichier guide tour pour la version anglaise; TODO: la traduction de chaque contenu d'aide - Création et configuration des raccourcis clavier pour les utilisateurs anglais

--- web/js/launch_site/front.js
+++ web/js/launch_site/front.js
@@ -38,75 +38,8 @@ function processScroll()
 	}
 }
 
-var	$btnCompare = $('a#btn-compare'),
-	$searchInputText = $('input[type="text"]#filter-value'),
-	$btnComparisonList = $('a#drop-comparison-list'),
-	$btnHelp = $('li.help-action'),
-	$btnFilter = $('a#drop-filter-list'),
-	isFirstActionAfterGT = false;
-
-function shortcutListener(event) {
-	if (isFirstActionAfterGT) {
-		isFirstActionAfterGT = false;
-		
-		return false;
-	}
-
-	switch(event.which) {
-		case 67: // touche 'c'
-			if ($btnCompare.hasClass('disabled')) {
-				return false;
-			}
-
-			$btnCompare.trigger('click');
-			break;
-		case 82: // touche 'r'
-			$('.dropdown.open .dropdown-toggle').dropdown('toggle');
-			$searchInputText[0].selectionStart = $searchInputText[0].selectionEnd = $searchInputText.val().length;
-			break;
-		case 76:
-			$btnComparisonList.trigger('click');
-			break;
-		case 70:
-			$btnFilter.trigger('click');
-			break;
-		case 86:
-			$('.dropdown.open .dropdown-toggle').dropdown('toggle');
-			$btnHelp.trigger('click');
-		default:
-			break;
-
-	}
-}
-
 $(document).ready(function()
 {	
-	$('body').bind('keyup', function(event) 
-	{
-		shortcutListener(event);
-	});
-
-	$('input[type="email"]#user_email, input[type="text"]#filter-value').on('focus', function()
-	{
-		$('body').unbind('keyup');
-		$searchInputText.bind('keyup', function(event)
-		{
-			if (event.which == 27) {
-				$(this).blur();
-			}
-		});
-	});
-
-	
-
-	$('input[type="email"]#user_email, input[type="text"]#filter-value').on('blur', function()
-	{
-		$('body').bind('keyup', function(event)
-		{
-			shortcutListener(event);
-		});
-	});
-
 	//On active les tooltips
 	$('#wrapper').on('mouseover', '.tooltip-anchor', function(){
 		$(this).tooltip('show');
