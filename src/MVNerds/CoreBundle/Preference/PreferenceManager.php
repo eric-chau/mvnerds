27d23faac89489c90efe823293cf9dc78e432bbc
Haniki <Haniki@Haniki-PC>
2012-11-19 18:36:32 +0100

    refix merge !!!!!!

--- src/MVNerds/CoreBundle/Preference/PreferenceManager.php
+++ src/MVNerds/CoreBundle/Preference/PreferenceManager.php
@@ -5,7 +5,7 @@ namespace MVNerds\CoreBundle\Preference;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 
 use MVNerds\CoreBundle\Model\PreferenceQuery;
-use MVNerds\CoreBundle\Model\PreferenceQuery;
+use MVNerds\CoreBundle\Model\PreferencePeer;
 use MVNerds\CoreBundle\Model\UserPreferenceQuery;
 use MVNerds\CoreBundle\Model\UserPreferencePeer;
 
