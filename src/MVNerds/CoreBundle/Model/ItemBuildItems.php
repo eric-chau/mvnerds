9c6f460b96d51ae2e32d79191acb951c01b0c547
haniki <hani.yagoub@gmail.com>
2012-11-22 11:32:11 +0100

    Ajout fichiers model item_build_items

---
+++ src/MVNerds/CoreBundle/Model/ItemBuildItems.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MVNerds\CoreBundle\Model;
+
+use MVNerds\CoreBundle\Model\om\BaseItemBuildItems;
+
+
+/**
+ * Skeleton subclass for representing a row from the 'item_build_items' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.src.MVNerds.CoreBundle.Model
+ */
+class ItemBuildItems extends BaseItemBuildItems {
+
+} // ItemBuildItems
