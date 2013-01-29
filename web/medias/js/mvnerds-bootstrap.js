2cd54d7a735f6785a41fe1881c72dd0282e3ecb3
Haniki <Haniki@Haniki-PC>
2013-01-29 15:15:11 +0100

    Fix bug ajout champions filtrés

--- web/medias/js/mvnerds-bootstrap.js
+++ web/medias/js/mvnerds-bootstrap.js
@@ -3,5 +3,12 @@ $(function() {
 	$('.dropdown-toggle').dropdown();
 	// Activation des popovers
 	$('.bootstrap-popover').popover();
+	//Blocage du clic des liens désactivés
+	$('a.disabled').on('click', function(){
+		if ($(this).hasClass('disabled')) {
+			return false;
+		}
+		return true;
+	});
 });
 
