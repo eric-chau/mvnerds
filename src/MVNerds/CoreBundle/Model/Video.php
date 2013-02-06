6fcdda3958d899f343469b6ed8a91c2dd2daa0cd
Haniki <Haniki@Haniki-PC>
2013-02-06 14:43:22 +0100

    Ajout de l aperçu des videos sur la page de listing

--- src/MVNerds/CoreBundle/Model/Video.php
+++ src/MVNerds/CoreBundle/Model/Video.php
@@ -18,4 +18,33 @@ use MVNerds\CoreBundle\Model\om\BaseVideo;
  */
 class Video extends BaseVideo {
 
+	public function getThumbnailUrl()
+	{
+		if (strpos($this->getLink(), 'youtube.com') !== false) { 
+			$link = 'http://img.youtube.com/vi/';
+			$exploded = explode('&', str_replace('http://www.youtube.com/watch?v=', '', $this->getLink()));
+			
+			return $link . $exploded[0] . '/0.jpg';
+		} elseif (strpos($this->getLink(), 'youtu.be') !== false) {
+			$link = 'http://img.youtube.com/vi/';
+			$exploded = explode('&', str_replace('http://youtu.be/', '', $this->getLink()));
+			
+			return $link . $exploded[0] . '/0.jpg';
+		} elseif (strpos($this->getLink(),'dailymotion.com') !== false) {
+			$link = 'http://www.dailymotion.com/thumbnail/video/';
+			if (strpos($this->getLink(),'/video/') !== false) {
+				$exploded = explode('_', str_replace('http://www.dailymotion.com/video/', '', $this->getLink()));
+			
+				return $link . $exploded[0];
+			} elseif (strpos($this->getLink(),'#video=') !== false) {
+				$exploded = preg_replace('/http:\/\/www\.dailymotion\.com\/.*#video=/', '', $this->getLink());
+				
+				return $link . $exploded;
+			} else {
+				return '';
+			}
+		} else {
+			return '';
+		}
+	}
 } // Video
