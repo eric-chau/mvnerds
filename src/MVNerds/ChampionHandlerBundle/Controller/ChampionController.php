40c066f6cc2ce713fc0f01c89a281452d5f1aeba
Haniki <Haniki@Haniki-PC>
2013-02-11 16:16:31 +0100

    Mise en fonctionnement du champ de recherche de champions dans le header

--- src/MVNerds/ChampionHandlerBundle/Controller/ChampionController.php
+++ src/MVNerds/ChampionHandlerBundle/Controller/ChampionController.php
@@ -15,16 +15,30 @@ class ChampionController extends Controller
 	 */
 	public function championDetailAction($slug)
 	{
-		$champion = null;
 		try {
 			$champion = $this->get('mvnerds.champion_manager')->findBySlugWithSkillsAndSkins($slug);
-		}
-		catch (Exception $e) {
-			
+		} catch (Exception $e) {
+			return $this->redirect($this->generateUrl('site_homepage'));
 		}		
 		
 		return $this->render('MVNerdsChampionHandlerBundle:Champion:champion_detail.html.twig', array(
 			'champion' => $champion
 		));
+	}
+	
+	/**
+	 * Retourne la page de détail d'un champion en se basant sur son nom
+	 * 
+	 * @Route("/champion-by-name/{name}", name="champion_detail_by_name", options={"expose"=true})
+	 */
+	public function championDetailByNameAction($name)
+	{
+		try {
+			$champion = $this->get('mvnerds.champion_manager')->findByName($name);
+		} catch (Exception $e) {
+			return $this->redirect($this->generateUrl('site_homepage'));
+		}		
+		
+		return $this->redirect($this->generateUrl('champion_detail', array('slug' => $champion->getSlug())));
 	}
 }
