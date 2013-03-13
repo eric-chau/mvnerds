2aeccd2f440aeb16b936397159b2680bfa3eb624
Haniki <Haniki@Haniki-PC>
2013-03-13 14:27:55 +0100

    ajout de la gestion des slug automatique pour les i18n ajoutés manuellement

--- src/MVNerds/CoreBundle/Model/SkillI18n.php
+++ src/MVNerds/CoreBundle/Model/SkillI18n.php
@@ -18,4 +18,57 @@ use MVNerds\CoreBundle\Model\om\BaseSkillI18n;
  */
 class SkillI18n extends BaseSkillI18n {
 
+	protected $skillSlug;
+	
+	public function setName($name) 
+	{
+		//parent::setName($name);
+		
+		if ($name !== null && is_numeric($name)) {
+			$name = (string) $name;
+		}
+		
+		//Commenté pour simplifier la correction des bugs de skills à remettre plus tard
+		//if ($this->name !== $v) {
+			$this->name = $name;
+			$this->modifiedColumns[] = SkillI18nPeer::NAME;
+		//}
+			
+		if ('fr' == $this->getLang()) {
+			$in = array(
+				'/[éèê]/u',
+				'/[àâ]/u',
+				'/[ïî]/u',
+				'/[ç]/u',
+				'/[^\w]+/u'
+			);
+			
+			$out = array(
+				'e',
+				'a',
+				'i',
+				'c',
+				'-'
+			);
+			
+			$slug = preg_replace($in, $out, mb_strtolower($this->getName(), 'UTF-8'));
+			if (( $skill = $this->getSkill())) {
+				$skill->setSlug($slug);
+			} else {
+				$this->skillSlug = $slug;
+			}
+			
+		}
+
+		  return $this;
+	}
+	
+	public function setSkill(Skill $v = null)
+	{
+		if ($this->skillSlug != null) {
+			$v->setSlug($this->skillSlug);
+		}
+		
+		parent::setSkill($v);
+	}
 } // SkillI18n
