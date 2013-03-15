010693f7c4ecae67aed1c4e8505b7dee3c532c17
Haniki <Haniki@Haniki-PC>
2013-03-15 11:48:19 +0100

    Ajout de l'upload d'images pour les news.

--- src/MVNerds/CoreBundle/Model/News.php
+++ src/MVNerds/CoreBundle/Model/News.php
@@ -2,6 +2,8 @@
 
 namespace MVNerds\CoreBundle\Model;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 use MVNerds\CoreBundle\Comment\IComment;
 use MVNerds\CoreBundle\Model\om\BaseNews;
 
@@ -18,4 +20,32 @@ use MVNerds\CoreBundle\Model\om\BaseNews;
  */
 class News extends BaseNews {
 
+	public function getImage()
+	{
+		return '';
+	}
+	
+	/**
+	 * ATTENTION L'EXTENSION PHP_FILEINFO DOIT ETRE
+	 * ACTIVEE POUR POUVOIR UTILISER LA METHODE 
+	 * GUESS_EXTENTION
+	 */
+	public function setImage(UploadedFile $v)
+	{
+		//On récupère l extension de l'image
+		$extension = $v->guessExtension();
+		if (!$extension) {
+			$extension = 'bin';
+		}
+		//on génère un nom aléatoire jusqu'a en trouver un qui n'existe pas encore
+		do {
+			$imageName = rand(1, 99999) . '.' . $extension;
+		} while (file_exists(__DIR__ . '/../../../../web/medias/images/news/' . $imageName));
+		
+		//On crée une copie de l'image en local
+		$v->move( __DIR__ . '/../../../../web/medias/images/news/' ,$imageName);
+		
+		//On set le champ en BDD pour l'image
+		$this->setImageName('/medias/images/news/' . $imageName);
+	}
 } // News
