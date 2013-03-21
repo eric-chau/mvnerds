f3e11667437b3684beb2da5427e1961c2fa2043c
Haniki <Haniki@Haniki-PC>
2013-03-21 15:30:34 +0100

    Ajout de la miniaturisation des images des news

--- src/MVNerds/CoreBundle/Model/News.php
+++ src/MVNerds/CoreBundle/Model/News.php
@@ -2,9 +2,6 @@
 
 namespace MVNerds\CoreBundle\Model;
 
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-
-use MVNerds\CoreBundle\Comment\IComment;
 use MVNerds\CoreBundle\Model\om\BaseNews;
 
 /**
@@ -37,17 +34,69 @@ class News extends BaseNews {
 			$extension = $v->guessExtension();
 			if (!$extension) {
 				$extension = 'bin';
-			}
+			}			
 			//on génère un nom aléatoire jusqu'a en trouver un qui n'existe pas encore
 			do {
 				$imageName = rand(1, 99999) . '.' . $extension;
 			} while (file_exists(__DIR__ . '/../../../../web/medias/images/news/' . $imageName));
 
 			//On crée une copie de l'image en local
-			$v->move( __DIR__ . '/../../../../web/medias/images/news/' ,$imageName);
+			$destination = __DIR__ . '/../../../../web/medias/images/news/';
+			$v->move( $destination ,$imageName);
 
+			//Création de la miniature
+			$source = null;
+			if ($extension == 'jpeg' || $extension == 'jpg' || $extension == 'jpe' || $extension == 'jfif') {
+				$source = imagecreatefromjpeg($destination . $imageName);
+				$miniature = $this->generateResampled($source, $imageName, $extension, $destination);
+				imagejpeg($miniature, $destination . "mini_" . $imageName);
+			} elseif ($extension == 'gif') {
+				$source = imagecreatefromgif($destination . $imageName);
+				$miniature = $this->generateResampled($source, $imageName, $extension, $destination);
+				imagegif($miniature, $destination . "mini_" . $imageName);
+			} elseif ($extension == 'png') {
+				$source = imagecreatefrompng($destination . $imageName);
+				$miniature = $this->generateResampled($source, $imageName, $extension, $destination);
+				imagepng($miniature, $destination . "mini_" . $imageName);
+			}
+			
+			if (($oldImageName = $this->getImageName())) {
+				if (file_exists($destination . $oldImageName)) {
+					unlink($destination . $oldImageName);
+				}
+				if (file_exists($destination . 'mini_' . $oldImageName)) {
+					unlink($destination . 'mini_' . $oldImageName);
+				}
+			}
+			
 			//On set le champ en BDD pour l'image
-			$this->setImageName('/medias/images/news/' . $imageName);
+			$this->setImageName($imageName);
 		}
+	}
+	
+	private function generateResampled($source)
+	{
+		$largeur_source = imagesx($source);
+		$hauteur_source = imagesy($source);
+
+		$maxHeight = 160;
+		$maxWidth = 300;
+		
+		$x = $maxWidth;
+		$y = $maxWidth * $hauteur_source / $largeur_source;
+		if ($y > $maxHeight) {
+			$y = $maxHeight;
+			$x = $largeur_source * $maxHeight / $hauteur_source;
+		}
+		
+		// On crée la miniature vide
+		$miniature = imagecreatetruecolor($x, $y); 
+		$largeur_miniature = imagesx($miniature);
+		$hauteur_miniature = imagesy($miniature);
+
+		// On crée la miniature
+		imagecopyresampled($miniature, $source, 0, 0, 0, 0, $largeur_miniature, $hauteur_miniature, $largeur_source, $hauteur_source);
+		
+		return $miniature;
 	}
 } // News
