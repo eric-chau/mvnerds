b97b56fc1e52abafe9b97e5ab239ae291c3d15ea
Haniki <Haniki@Haniki-PC>
2013-03-27 19:03:40 +0100

    Team-Seeker : Ajout de la vue, des actions de base du controller, le less, et le js

--- src/MVNerds/TeamSeekerBundle/Controller/TeamSeekerController.php
+++ src/MVNerds/TeamSeekerBundle/Controller/TeamSeekerController.php
@@ -4,6 +4,7 @@ namespace MVNerds\TeamSeekerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/team-seeker")
@@ -19,4 +20,20 @@ class TeamSeekerController extends Controller
     {
         return $this->render('::base.html.twig');
     }
+	
+	/**
+	 * @Route("/", name="team_seeker_index")
+	 */
+	public function indexAction()
+	{
+		return $this->render('MVNerdsTeamSeekerBundle:TeamSeeker:team_seeker_index.html.twig');
+	}
+	
+	/**
+	 * @Route("/seek", name="team_seeker_seek_ajax", options={"expose"=true})
+	 */
+	public function seekAction()
+	{
+		return new Response();
+	}
 }
---
+++ web/medias/js/team-seeker/team-seeker-index.js
@@ -0,0 +1,80 @@
+var $searchTeamButton;
+
+function seekTeam() {
+	/**
+	 * TO RO0NY regionVal et teamVal
+	 * CE SONT LES DEUX VARIABLES UTILISEES POUR EFFECTUER LA RECHERCHE
+	 * DES L ARRIVEE SUR LA PAGE
+	 * SO PLS SET LES DEUX VARS AVEC LA REGION ET LA TEAM
+	 */
+	if (typeof regionVal != 'undefined' && typeof teamVal != 'undefined') {
+		if (regionVal != '' && teamVal != '') {
+			$.ajax({
+				type: 'POST',
+				url:  Routing.generate('team_seeker_seek_ajax', {_locale: locale}),
+				data: data,
+				dataType: 'json'
+			}).done(function(){
+				console.log('done');
+			}).fail(function(){
+				console.log('fail');
+			})
+		} else {
+			console.log('not valid');
+		}
+	} else {
+		console.log('undefined');
+	}
+}
+
+$(document).ready(function() {
+	
+	$searchTeamButton = $('#team-seeker-submit');
+	
+	//Recherche lancée dès l'arrivée sur la page pour le cas ou la recherche n'a pas été faite depuis la page dédiée.
+	seekTeam();
+	
+	// Event d'écoute sur le change du texte sur le champ de texte #team-seeker-input
+	$('input#team-seeker-input').on('click keyup change', function(event)
+	{
+		if (event.which == 13) {
+			$searchTeamButton.trigger('click');
+		}
+
+		if ($.trim($(this).val()) != '') {
+			$searchTeamButton.removeClass('disabled');
+		} else {
+			$searchTeamButton.addClass('disabled');
+		}
+	});
+	
+	//Soumission du formulaire de recherche de team
+	$searchTeamButton.click(function(e) {
+		e.preventDefault();
+		
+		$('#team-seeker-spinner').removeClass('hide');
+		regionVal = $('#team-seeker-region-selector').val();
+		teamVal = $('#team-seeker-input').val();
+		
+		seekTeam();
+	});
+	
+	// Vérifie si c'est la première fois ou non que l'utilisateur accède au module Team Seeker
+	var howItWorksValue = getItemFromLS('display_how_it_works_team_seeker');
+	if (howItWorksValue == undefined || howItWorksValue == 'true') {
+		$('a.how-it-works-toggle').find('span.label').toggleClass('disabled');
+		$('div.how-it-works').slideDown();
+
+		if (howItWorksValue == undefined) {
+			saveItemInLS('display_how_it_works_team_seeker', false);
+		}
+	}
+	// Toggle du "comment ça marche ?"
+	$('a.how-it-works-toggle').on('click', function(event) {
+		event.stopPropagation();
+		$('div.how-it-works').slideToggle();
+		var $label = $(this).find('span.label');
+		$label.toggleClass('disabled');
+		saveItemInLS('display_how_it_works_team_seeker', $label.hasClass('disabled'));
+	});
+});
